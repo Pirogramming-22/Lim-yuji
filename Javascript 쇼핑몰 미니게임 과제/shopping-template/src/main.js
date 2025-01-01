73155cfb9e0a6ac2d29166fa4883f20e10871ff4
lim-yuji <limyuji01@naver.com>
2025-01-01 16:55:24 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -13,14 +13,32 @@ function createHTMLString(item) {
          <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
          <span class="item__description">${item.gender}, ${item.size}</span>
     </li>
-    `;
-    
+    `; 
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) {
+        return;
+    }
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 //main
 loadItems()
 .then(items => {
     displayItems(items);
-    //setEventListeners(items)
+    setEventListeners(items)
 
 })
 .catch(console.log)
